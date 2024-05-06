6ab88d4457cc8984fbc424a350231b477abcc688
Chiuzu-chilumbu <chiuzubenny@hotmail.com>
2024-05-06 11:34:18 +0800

    Competed personalised echo challenges

---
+++ Bash/looping_with_numbers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+# loop  from 1 to 99
+for i in {1..50}
+do
+  echo $i
+done
---
+++ Bash/personalised_echo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# create the name variable after obtaining the input using 'read'
+read name 
+# echo 'Welcome' + the name variable
+echo "Welcome $name"
+
